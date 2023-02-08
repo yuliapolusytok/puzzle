e14c3923afcfdf8a889ba985b77a5a5b20faeae3
Vladyslav Atamaniuk <115092059+H1mansh0@users.noreply.github.com>
2023-02-08 09:17:09 +0200

    Finished validate_checker

--- puzzle.py
+++ puzzle.py
@@ -7,5 +7,35 @@ def transformer(board):
 def blockchecker(board):
     pass
 
-def validate_board(board) -> bool:
-    pass
+def validate_board(board: list) -> bool:
+    """
+    Function checks if the board
+    has the same numbers in lines,
+    rows and same blocks
+    
+    >>> validate_board([
+        "**** ****",
+        "***1 ****",
+        "**  3****",
+        "* 4 1****",
+        "     9 5 ",
+        " 6  83  *",
+        "3   1  **",
+        "  8  2***",
+        "  2  ****"
+    ])
+    False
+    >>> validate_board([
+        "**** ****",
+        "***1 ****",
+        "**  3****",
+        "* 4 1****",
+        "     9 5 ",
+        " 6  83  *",
+        "3   6  **",
+        "  8  2***",
+        "  2  ****"
+    ])
+    True
+    """
+    return gorizontal_checking(board) and transformer(board) and blockchecker(board)
