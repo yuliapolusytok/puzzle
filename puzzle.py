3cb5512bad0027306f92771405303a569ebd95dd
PavloKukurik <116553132+PavloKukurik@users.noreply.github.com>
2023-02-08 09:00:18 +0200

    Update puzzle.py

--- puzzle.py
+++ puzzle.py
@@ -1,9 +1,9 @@
-def check_digits(board: list[str]) -> bool:
+def gorizontal_checking(board: list[str]) -> bool:
     """
     The function do check if elem in list have more than one similar digt
     :param board: board of game
     :return: bool
-    >>> check_digits([ "**** ****", "***1 ****", "**  3****", "* 4 1****", "     9 5 "," 6  83  *", "3   1  **", "  8  2***", "  2  ****"])
+    >>> gorizontal_checking([ "**** ****", "***1 ****", "**  3****", "* 4 1****", "     9 5 "," 6  83  *", "3   1  **", "  8  2***", "  2  ****"])
     False
     """
     for i in board:
